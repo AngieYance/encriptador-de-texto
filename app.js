f35be71dec6d08b17f17c223843f8a7500992ca8
Angie Yance <angieyance70@gmail.com>
2024-08-27 15:06:14 -0500

    Creación de la funcionalidad de desencriptar texto

--- app.js
+++ app.js
@@ -24,4 +24,31 @@ function encriptar(){
  }
 }
 
+function desencriptar(){
+  let texto = document.getElementById ("texto").value;
+  let tituloMensaje = document.getElementById ("titulo__mensaje");
+  let parrafo = document.getElementById ("parrafo");
+  let ilustracion = document.getElementById("Muñeco"); 
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u"); 
+
+    if (texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        ilustracion.src ="./Img/Muñeco 3.png";
+    } else{
+        ilustracion.src = "./Img/Muñeco.png"; 
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert ('Debes ingresar algún texto');
+    }
+}
+
+desencriptar()
 encriptar()
